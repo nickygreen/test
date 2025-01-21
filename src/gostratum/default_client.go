fd244613e55cd5724b318279b4e0867504e600b7
nickygreen <cukrowski.m@gmail.com>
2025-01-21 10:37:33 +0100

    Update default_client.go

--- src/gostratum/default_client.go
+++ src/gostratum/default_client.go
@@ -1,4 +1,4 @@
-package gostratum
+trepackage gostratum
 
 import (
 	"fmt"
@@ -111,20 +111,20 @@ func SendExtranonce(ctx *StratumContext) {
 	}
 }
 
-var walletRegex = regexp.MustCompile("kaspa:[a-z0-9]+")
+var walletRegex = regexp.MustCompile("spectre:[a-z0-9]+")
 
 func CleanWallet(in string) (string, error) {
-	_, err := util.DecodeAddress(in, util.Bech32PrefixKaspa)
+	_, err := util.DecodeAddress(in, util.Bech32PrefixSpectre)
 	if err == nil {
 		return in, nil // good to go
 	}
-	if !strings.HasPrefix(in, "kaspa:") {
-		return CleanWallet("kaspa:" + in)
+	if !strings.HasPrefix(in, "spectre:") {
+		return CleanWallet("spectre:" + in)
 	}
 
 	// has kaspa: prefix but other weirdness somewhere
 	if walletRegex.MatchString(in) {
 		return in[0:67], nil
 	}
-	return "", errors.New("unable to coerce wallet to valid kaspa address")
+	return "", errors.New("unable to coerce wallet to valid spectre address")
 }
